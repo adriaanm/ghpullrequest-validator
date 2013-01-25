8c61bac6d583f8324dbf2c0f2749489211b1f739
Adriaan Moors <adriaan.moors@typesafe.com>
2013-01-24 16:55:01 -0800

    get rid of shady backoff -- race in mutation?

--- src/main/scala/backend/JenkinsJobWatcher.scala
+++ src/main/scala/backend/JenkinsJobWatcher.scala
@@ -8,20 +8,12 @@ import akka.util.duration._
 /** An actor that watches a specific job and returns its status when the job is completed. */
 class JenkinsJobWatcher(api: JenkinsAPI, build: BuildProject, buildnumber: String, watcher: ActorRef) extends Actor with ActorLogging {
   log.debug("Watching for job finish: " + buildnumber)
-  private var _backoff = 1
-  def backoff: Int =
-    if (_backoff >= 8) 8
-    else {
-      val res = _backoff
-      _backoff *= 2
-      res
-    }
 
   def receive: Receive = {
     case ReceiveTimeout => 
       val status = jobStatus
-      log.debug("Job finished? " + build.job.name + "-" + status + " building: "+ status.building + "current backoff: "+ _backoff)
-      if (status.building) context setReceiveTimeout (backoff minutes)
+      log.debug("Job finished? " + build.job.name + "-" + status + " building: "+ status.building)
+      if (status.building) context setReceiveTimeout (5 minutes)
       else {
         log.debug("Job finished! " + build.job.name)
         build.watcher ! BuildResult(status.isSuccess, status.url)
